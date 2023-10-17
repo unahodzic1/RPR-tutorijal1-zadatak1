215f0a6ab37a94b2732363f1705029a4f656218b
Una Hodžić <uhodzic1@etf.unsa.ba>
2023-10-17 20:04:36 +0200

    Init

--- src/Zadatak1.java
+++ src/Zadatak1.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Zadatak1 {
     public static void main(String[] args){
-        System.out.println("Hello world!");
         int a, b;
         Scanner unos=new Scanner(System.in);
         System.out.println("Unesite prvi broj: ");
