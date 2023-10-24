94a79fb6271c91c766e837e7fec87624b3556048
Una Hodžić <uhodzic1@etf.unsa.ba>
2023-10-24 18:55:25 +0200

    Zadatak 2 (nije dovrsen)

--- src/ba/unsa/etf/rpr/lab2/zadatak2/Korisnik.java
+++ src/ba/unsa/etf/rpr/lab2/zadatak2/Korisnik.java
@@ -1,7 +1,38 @@
 package ba.unsa.etf.rpr.lab2.zadatak2;
 
+import java.util.Objects;
+
 public class Korisnik extends Osoba{
+
+    private Racun racun;
+
     public Korisnik(String ime, String prezime) {
         super(ime, prezime);
+    }
+
+    public Racun getRacun() {
+        return racun;
+    }
+
+    public void setRacun(Racun racun) {
+        this.racun = racun;
+    }
+
+    public void dodajRacun(Racun racun){
+        this.racun = racun;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Korisnik)) return false;
+        if (!super.equals(o)) return false;
+        Korisnik korisnik = (Korisnik) o;
+        return Objects.equals(getRacun(), korisnik.getRacun());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getRacun());
     }
 }
--- src/ba/unsa/etf/rpr/lab2/zadatak2/Racun.java
+++ src/ba/unsa/etf/rpr/lab2/zadatak2/Racun.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rpr.lab2.zadatak2;
 
 public class Racun {
+
+    private Double prekoracenje; // potrebno zbog metode odobriPrekoracenje
+    private Long brojRacuna;
+    private Osoba korisnikRacuna;
+    private boolean odobrenjePrekoracenja;
+    private Double stanjeRacuna;
+
+    public Racun(Long brojRacuna, Osoba korisnikRacuna) {
+        this.brojRacuna = brojRacuna;
+        this.korisnikRacuna = korisnikRacuna;
+    }
+
+    /*
+    public boolean provjeriOdobrenjePrekoracenja(Double prekoracenje){
+
+    }
+    */
+
+    public boolean izvrsiUplatu(Double iznos){
+        if(this.stanjeRacuna < iznos) return false;
+        else {
+            this.stanjeRacuna = this.stanjeRacuna - iznos;
+            return true;
+        }
+    }
+
+    public boolean izvrsiIsplatu(Double iznos){
+        this.stanjeRacuna = this.stanjeRacuna + iznos;
+        return true;
+    }
+
+    public void odobriPrekoracenje(Double prekoracenje){
+        this.prekoracenje = prekoracenje;
+    }
 }
