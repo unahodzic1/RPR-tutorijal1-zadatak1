4cda9ef38e532f867c77eba8b5c8bb7e3db29355
unahodzic1 <uhodzic1@etf.unsa.ba>
2023-11-14 00:16:38 +0100

    Main uradjen

--- src/main/java/ba/unsa/etf/rpr/lv4/App.java
+++ src/main/java/ba/unsa/etf/rpr/lv4/App.java
@@ -7,30 +7,55 @@ public class App
     public static void main( String[] args )
     {
 
-        File file = new File("laptopi.json");
-        LaptopDaoJSONFile laptopDao = new LaptopDaoJSONFile(file);
-        
-        Laptop laptop1 = new Laptop("Asus", "...", 0, 0, 0, 0, "...", "...", 0);
-        Laptop laptop2 = new Laptop("Dell", "...", 1000, 0, 0, 0, "...", "...", 0);
+        Laptop laptop1 = new Laptop("Asus", "...", 3000, 0, 0, 0, "...", "...", 12);
+        Laptop laptop2 = new Laptop("Dell", "...", 1000, 0, 0, 0, "...", "...", 16);
 
-        laptopDao.dodajLaptopUListu(laptop1);
-        laptopDao.dodajLaptopUListu(laptop2);
+        // JSON
 
-        laptopDao.dodajLaptopUFile(laptop1);
-        laptopDao.dodajLaptopUFile(laptop2);
+        File file1 = new File("laptopi.json");
+        LaptopDaoJSONFile laptopDao1 = new LaptopDaoJSONFile(file1);
 
-        ArrayList<Laptop> listaIzDatoteke = laptopDao.vratiPodatkeIzDatoteke();
+        laptopDao1.dodajLaptopUListu(laptop1);
+        laptopDao1.dodajLaptopUListu(laptop2);
+
+        laptopDao1.dodajLaptopUFile(laptop1);
+        laptopDao1.dodajLaptopUFile(laptop2);
+
+        ArrayList<Laptop> listaIzDatoteke1 = laptopDao1.vratiPodatkeIzDatoteke();
         System.out.println("Podaci iz datoteke:");
-        for (Laptop laptop : listaIzDatoteke) {
+        for (Laptop laptop : listaIzDatoteke1) {
             System.out.println(laptop);
         }
 
-        // Pretraživanje laptopa po procesoru
         try {
-            Laptop trazeniLaptop = laptopDao.getLaptop("Procesor1");
-            System.out.println("Pronađeni laptop: " + trazeniLaptop);
+            Laptop trazeniLaptop1 = laptopDao1.getLaptop("Procesor1");
+            System.out.println("Pronađeni laptop: " + trazeniLaptop1);
+        } catch (NeodgovarajuciProcesorException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // XML
+
+        File file2 = new File("laptopi.xml");
+        LaptopDaoXMLFile laptopDao2 = new LaptopDaoXMLFile(file2);
+
+        laptopDao2.dodajLaptopUListu(laptop1);
+        laptopDao2.dodajLaptopUListu(laptop2);
+
+        laptopDao2.dodajLaptopUFile(laptop1);
+        laptopDao2.dodajLaptopUFile(laptop2);
+
+        ArrayList<Laptop> listaIzDatoteke2 = laptopDao2.vratiPodatkeIzDatoteke();
+        System.out.println("Podaci iz datoteke:");
+        for (Laptop laptop : listaIzDatoteke2) {
+            System.out.println(laptop);
+        }
+
+        try {
+            Laptop trazeniLaptop2 = laptopDao2.getLaptop("Procesor1");
+            System.out.println("Pronađeni laptop: " + trazeniLaptop2);
         } catch (NeodgovarajuciProcesorException e) {
             System.out.println(e.getMessage());
         }
     }
-}
+    }
