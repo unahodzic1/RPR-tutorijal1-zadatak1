b06d02afc46cd3edf2f087f96284a97d2adc860c
unahodzic1 <uhodzic1@etf.unsa.ba>
2024-01-08 14:02:46 +0100

    Dodani testovi

--- src/main/test/com/example/lv1011/GeografijaDAOTest.java
+++ src/main/test/com/example/lv1011/GeografijaDAOTest.java
@@ -1,5 +1,6 @@
 package com.example.lv1011;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.beans.Statement;
 import java.util.ArrayList;
@@ -8,23 +9,58 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GeografijaDAOTest {
 
-    private GeografijaDAO dao;
+    private static GeografijaDAO dao;
+
+    @BeforeAll
+    public static void konekcija(){
+        dao = GeografijaDAO.getInstance();
+    }
 
     // testovi koji mijenjaju stanje baze
 
     @Test
     public void testDodajGrad() {
-        dao = GeografijaDAO.getInstance();
-
+        Grad grad = new Grad(5, "Zenica", 150, 1);
+        dao.dodajGrad(grad);
+        assertNotNull(dao.nadjiGrad("Zenica"));
     }
 
-    // DOVRSITI
+    @Test
+    public void testObrisiGrad() {
+        dao.obrisiGrad("Zenica");
+        assertNull(dao.nadjiGrad("Zenica"));
+    }
+
     @Test
     public void testDodajDrzavu() {
-        dao = GeografijaDAO.getInstance();
-        Drzava drzava = new Drzava(4, "Somalija", 2);
+        Drzava drzava = new Drzava(4, "Somalija", 1);
         dao.dodajDrzavu(drzava);
-        assertEquals(dao.nadjiDrzavu(drzava.getNaziv()).getNaziv(), "Somalija");
+        assertNotNull(dao.nadjiDrzavu("Somalija"));
+    }
+
+    @Test
+    public void testObrisiDrzavu() {
+        dao.obrisiDrzavu("Somalija");
+        assertNull(dao.nadjiDrzavu("Somalija"));
+    }
+
+    // testovi koji ne mijenjaju stanje baze
+
+    @BeforeAll
+    public static void resetuj() {
+        GeografijaDAO.getInstance().obrisiSveUnose();
+    }
+
+    @Test
+    public void testDodajObrisiDrzavu(){
+        Drzava drzava = new Drzava(4, "Srbija", 2);
+        dao.dodajDrzavu(drzava);
+        assertEquals("Srbija", dao.nadjiDrzavu("Srbija").getNaziv());
+    }
+
+    @Test
+    public void testProvjeri(){
+        assertNull(dao.nadjiDrzavu("Srbija"));
     }
 
 }
